e268bb5e3aa53a65d3a7a6912edb8ccb1404475a
Gaurang Kumar <gaurangkumarp@gmail.com>
2019-06-09 00:16:40 +0530

    routes added

--- app/Route/web.php
+++ app/Route/web.php
@@ -16,58 +16,23 @@ use Friday\Http\Route;
  *
  */
 
-# Route with controller@method. IndexController@Index is default
-Route::get('/');
-Route::get('/index2', 'IndexController');
-Route::get('/index3', 'IndexController@Index');
+#index
+Route::get('/', 'IndexController@index');
 
-# Available Router Methods
-//Route::get($uri, $callback);
-//Route::post($uri, $callback);
+#show
+Route::get('/{id}', 'IndexController@show');
 
-# Route with optional multiple arguments - must pass default argument to all optioanl arguments
-Route::get('/member/{name}/{?id}', function ($name, $id = 1) {
-    echo "Name: [$id] $name";
-    echo '<br>Num of Args: '.func_num_args();
-    echo '<br>Args: ';print_r(func_get_args());
-});
+#create
+Route::get('/create', 'IndexController@create');
 
-# Route with optional arguments - must pass default  argument
-Route::get('/user/{?name}/', function ($name = 'GK') {
-    echo "Name: $name";
-    echo '<br>Num of Args: '.func_num_args();
-    echo '<br>Args: ';print_r(func_get_args());
-});
+#store
+Route::post('/', 'IndexController@store');
 
-# Route with multiple arguments - must be in sequence
-Route::get('/names/{id}/{name}', function ($id, $name) {
-    echo "Name: [$id] $name";
-});
+#show
+Route::get('/{id}/edit', 'IndexController@edit');
 
-# Route with arguments
-Route::get('/name/{name}', function ($name) {
-    echo 'Name: '.$name;
-});
+#store
+Route::post('/{id}/update', 'IndexController@update');
 
-# Route with view only (always GET method)
-Route::view('/view', 'index', ['name' => 'IronPHP']);
-
-# Route with callable
-Route::get('/callable', function () {
-    echo 'callable';
-});
-
-# Route with callable
-Route::post('/post', function () {
-    echo 'post';
-});
-
-# Route with rediect
-Route::get('foo', function () {
-    echo 'Hello';
-    return ' World';
-});
-
-# Redirect Routes
-Route::redirect('/here', '/there');
-//Route::redirect('/here', '/there', 301);
+#show
+Route::get('/{id}/delete', 'IndexController@destroy');
